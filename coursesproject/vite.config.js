eb84ac084aae5c9ebdcff03b401897646d57f11c
miraeeeee <raynalllna70@gmail.com>
2025-10-06 13:39:29 +0700

    init2

--- coursesproject/vite.config.js
+++ coursesproject/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // load .env sesuai mode (development/production)
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    build: {
+      outDir: "../public_html", // folder hasil build
+    },
+    define: {
+      DELCOM_BASEURL: JSON.stringify(env.VITE_DELCOM_BASEURL || ""),
+    },
+  };
+});
